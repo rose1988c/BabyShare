24af64c30c623890b23462b30599f236628879eb
Vanilla <rose1988.c@gmail.com>
2014-08-07 21:16:53 +0800

    create project

--- app/config/dev/database.php
+++ app/config/dev/database.php
@@ -5,7 +5,7 @@ return array (
             'driver' => 'mysql',
             'host' => 'localhost',
             'port' => 3306,
-            'database' => 'mcc_manage',
+            'database' => 'babyshare',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
