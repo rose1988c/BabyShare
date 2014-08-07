ab8b5b8025cfc043bdd7ac3e55a74dc3d1e90d14
Vanilla <rose1988.c@gmail.com>
2014-08-07 20:45:58 +0800

    create project

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+class HomeController extends BaseController
+{
+    protected $layout = 'layouts.index';
+    
+    public function index()
+    {
+        $this->layout->with('title', 'index');
+        $this->layout->content = View::make('index');
+    }
+}
